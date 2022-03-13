429a7f40d6236138a0d84d49501caf19319460ea
Chris Laine <christopher.ian.laine@gmail.com>
2022-03-13 14:04:41 +1300

    Cleanup of the dotnet 5 version of the blazor app

--- src/MyBlazorServer.Dotnet5/Startup.cs
+++ src/MyBlazorServer.Dotnet5/Startup.cs
@@ -1,18 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MyBlazorServer.Configuration;
-using MyBlazorServer.Data;
 
-namespace MyBlazorServer
+namespace MyBlazorServer.Dotnet5
 {
     public class Startup
     {
@@ -29,10 +21,8 @@ namespace MyBlazorServer
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
             services.AddHttpContextAccessor();
-
-            services.AddScoped<StateContainer>();
+            
 
         }
 
--- src/create-blazor-sln.sh
+++ src/create-blazor-sln.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dotnet new blazorserver --name MyBlazorServer
-dotnet new Sln --name LearnBlazor.Server.sln
+dotnet new Sln --name LearnBlazor.UrlMapping
 dotnet sln add MyBlazorServer/MyBlazorServer.csproj
